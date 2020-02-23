efa50d7b47f93db5b71dd6e1dd86c59246c30542
kumar ravi <hi@kumarravi.me>
2020-02-23 12:21:28 +0530

    added initial files
    Signed-off-by: kumar ravi <hi@kumarravi.me>

---
+++ infrastructure/DeployLocal.bat
@@ -0,0 +1,7 @@
+docker build -t legacy -f ../infrastructure/Dockerfile ../ --no-cache
+
+docker run -ti -d -p 8765:80 legacy:latest /bin/bash
+
+::start firefox.exe http://localhost:8765/
+start chrome http://localhost:8765/
+::open http://localhost:8765/
---
+++ infrastructure/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+echo 'welcome' >> /var/www/html/welcome.php
